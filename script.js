7759ec4b6eec351a0a4cc0171c3f66459f1e62b0
csilv19 <cleberdasilva0794@gmail.com>
2023-11-14 21:28:00 -0300

    Classificador de nível de Heroi

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-//# 1️⃣ Desafio Classificador de nível de Herói
-//**O Que deve ser utilizado**
-
-//- Variáveis
-//- Operadores
-//- Laços de repetição
-//- Estruturas de decisões
-
-//## Objetivo
-//Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-
-//Se XP for menor do que 1.000 = Ferro
-//Se XP for entre 1.001 e 2.000 = Bronze
-//Se XP for entre 2.001 e 5.000 = Prata
-//Se XP for entre 6.001 e 7.000 = Ouro
-//Se XP for entre 7.001 e 8.000 = Platina
-//Se XP for entre 8.001 e 9.000 = Ascendente
-//Se XP for entre 9.001 e 10.000= Imortal
-//Se XP for maior ou igual a 10.001 = Radiante
-
-//## Saída
-
-//Ao final deve se exibir uma mensagem:
-//"O Herói de nome **{nome}** está no nível de **{nivel}**"
-
-// Aqui começa o desafio !! 
-
 let nomeDoheroi = "Super Nathanzinho"
 let nivelDexp = 10000
 
@@ -57,3 +30,30 @@ while (nivelDexp >= 10001){
 }
 
 console.log("O Herói " + nomeDoheroi, "está no nível de "+nivelDexp,"")
+
+//# 1️⃣ Desafio Classificador de nível de Herói
+//**O Que deve ser utilizado**
+
+//- Variáveis
+//- Operadores
+//- Laços de repetição
+//- Estruturas de decisões
+
+//## Objetivo
+//Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+//Se XP for menor do que 1.000 = Ferro
+//Se XP for entre 1.001 e 2.000 = Bronze
+//Se XP for entre 2.001 e 5.000 = Prata
+//Se XP for entre 6.001 e 7.000 = Ouro
+//Se XP for entre 7.001 e 8.000 = Platina
+//Se XP for entre 8.001 e 9.000 = Ascendente
+//Se XP for entre 9.001 e 10.000= Imortal
+//Se XP for maior ou igual a 10.001 = Radiante
+
+//## Saída
+
+//Ao final deve se exibir uma mensagem:
+//"O Herói de nome **{nome}** está no nível de **{nivel}**"
+
+// Aqui começa o desafio !! 
