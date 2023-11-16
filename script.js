562daa507a0afd6aa7e2ce653adb992b30828931
Cleber Rodrigues <147222070+csilv19@users.noreply.github.com>
2023-11-16 19:05:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,25 @@
-let nomeDoheroi = "Super Nathanzinho"
-let nivelDexp = 10000
+let nomeDoheroi = "Super Nathanzinho";
+let nivelDexp = 10000;
+let nivel = "";
 
-while (nivelDexp <= 1){
-    nivelDexp = "Noob Master"
-}
-while (nivelDexp <= 1000) { 
-        nivelDexp = "Ferro"
-    }
-while (nivelDexp <= 2000){
-        nivelDexp = "Bronze"
-}
-while (nivelDexp <= 5000){
-    nivelDexp = "Prata"
-}
-while (nivelDexp <= 7000){
-    nivelDexp = "Ouro"
-}
-while (nivelDexp <= 8000){
-    nivelDexp = "Platina"
-}
-while (nivelDexp <= 9000){
-    nivelDexp = "Ascendente"
-}
-while (nivelDexp <= 10000){
-    nivelDexp = "Imortal"
-}
-while (nivelDexp >= 10001){
-    nivelDexp = "Radiante"
+if (nivelDexp <= 1000) {
+    nivel = "Noob Master";
+} else if (nivelDexp <= 2000) {
+    nivel = "Ferro";
+} else if (nivelDexp <= 5000) {
+    nivel = "Bronze";
+} else if (nivelDexp <= 7000) {
+    nivel = "Prata";
+} else if (nivelDexp <= 8000) {
+    nivel = "Ouro";
+} else if (nivelDexp <= 9000) {
+    nivel = "Platina";
+} else if (nivelDexp <= 10000) {
+    nivel = "Ascendente";
+} else if (nivelDexp >= 10001) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
 }
 
-console.log("O Herói " + nomeDoheroi, "está no nível de "+nivelDexp,"")
-
-//# 1️⃣ Desafio Classificador de nível de Herói
-//**O Que deve ser utilizado**
-
-//- Variáveis
-//- Operadores
-//- Laços de repetição
-//- Estruturas de decisões
-
-//## Objetivo
-//Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-
-//Se XP for menor do que 1.000 = Ferro
-//Se XP for entre 1.001 e 2.000 = Bronze
-//Se XP for entre 2.001 e 5.000 = Prata
-//Se XP for entre 6.001 e 7.000 = Ouro
-//Se XP for entre 7.001 e 8.000 = Platina
-//Se XP for entre 8.001 e 9.000 = Ascendente
-//Se XP for entre 9.001 e 10.000= Imortal
-//Se XP for maior ou igual a 10.001 = Radiante
-
-//## Saída
-
-//Ao final deve se exibir uma mensagem:
-//"O Herói de nome **{nome}** está no nível de **{nivel}**"
-
-// Aqui começa o desafio !! 
+console.log("O Herói " + nomeDoheroi + " está no nível de " + nivel);
